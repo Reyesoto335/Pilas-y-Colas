e8f59e78188ee0e88fb0a5d55827a92c4671dfc2
Reyesoto335 <elmersoto123@gmail.com>
2022-04-13 09:18:34 -0700

    Subida de código para el tercer ejercicio de invertir un String

---
+++ Ejercicio_3/Nodo_Invertido.java
@@ -0,0 +1,14 @@
+package Ejercicios_hoja;
+
+public class NodoPilaInvertida {
+    String dato;
+    NodoPilaInvertida siguiente;
+
+    public NodoPilaInvertida(String dato) {
+        this.dato = dato;
+        siguiente = null;
+    }
+    
+    
+    
+}
